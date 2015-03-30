de54620cb733e1795b6a1c0a7af25f84bab70e7d
Dustin <dsellinger@gmail.com>
2015-03-30 10:32:37 -0700

    Updating int to unsigned
    Just keeping up with main emonlib branch.

--- firmware/EmonLib.cpp
+++ firmware/EmonLib.cpp
@@ -14,7 +14,7 @@
 //--------------------------------------------------------------------------------------
 // Sets the pins to be used for voltage and current sensors
 //--------------------------------------------------------------------------------------
-void EnergyMonitor::voltage(int _inPinV, double _VCAL, double _PHASECAL)
+void EnergyMonitor::voltage(unsigned int _inPinV, double _VCAL, double _PHASECAL)
 {
    inPinV = _inPinV;
    VCAL = _VCAL;
@@ -22,7 +22,7 @@ void EnergyMonitor::voltage(int _inPinV, double _VCAL, double _PHASECAL)
    offsetV = ADC_COUNTS>>1;
 }
 
-void EnergyMonitor::current(int _inPinI, double _ICAL)
+void EnergyMonitor::current(unsigned int _inPinI, double _ICAL)
 {
    inPinI = _inPinI;
    ICAL = _ICAL;
@@ -31,16 +31,16 @@ void EnergyMonitor::current(int _inPinI, double _ICAL)
 
 //--------------------------------------------------------------------------------------
 // emon_calc procedure
-// Calculates realPower,apparentPower,powerFactor,Vrms,Irms,kwh increment
+// Calculates realPower,apparentPower,powerFactor,Vrms,Irms,kWh increment
 // From a sample window of the mains AC voltage and current.
 // The Sample window length is defined by the number of half wavelengths or crossings we choose to measure.
 //--------------------------------------------------------------------------------------
-void EnergyMonitor::calcVI(int crossings, unsigned int timeout)
+void EnergyMonitor::calcVI(unsigned int crossings, unsigned int timeout)
 {
 	int SUPPLYVOLTAGE=3300;
 
-  int crossCount = 0;                             //Used to measure number of times threshold is crossed.
-  int numberOfSamples = 0;                        //This is now incremented  
+  unsigned int crossCount = 0;                             //Used to measure number of times threshold is crossed.
+  unsigned int numberOfSamples = 0;                        //This is now incremented  
 
   //-------------------------------------------------------------------------------------------------------------------------
   // 1) Waits for the waveform to be close to 'zero' (500 adc) part in sin curve.
@@ -57,7 +57,7 @@ void EnergyMonitor::calcVI(int crossings, unsigned int timeout)
   }
   
   //-------------------------------------------------------------------------------------------------------------------------
-  // 2) Main measurment loop
+  // 2) Main measurement loop
   //------------------------------------------------------------------------------------------------------------------------- 
   start = millis(); 
 
@@ -121,7 +121,7 @@ void EnergyMonitor::calcVI(int crossings, unsigned int timeout)
   // 3) Post loop calculations
   //------------------------------------------------------------------------------------------------------------------------- 
   //Calculation of the root of the mean of the voltage and current squared (rms)
-  //Calibration coeficients applied. 
+  //Calibration coefficients applied. 
   
   double V_RATIO = VCAL *((SUPPLYVOLTAGE/1000.0) / (ADC_COUNTS));
   Vrms = V_RATIO * sqrt(sumV / numberOfSamples); 
@@ -187,4 +187,3 @@ void EnergyMonitor::serialprint()
     Serial.println(' ');
     delay(100); 
 }
-
